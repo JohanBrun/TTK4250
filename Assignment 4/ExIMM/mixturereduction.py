36e6f3681672bd503487c67baa3077654aa5efb8
Johan Brun <johanbrun@dhcp-10-22-33-62.wlan.ntnu.no>
2020-09-28 17:17:24 +0200

    Init repo

---
+++ Assignment 4/ExIMM/mixturereduction.py
@@ -0,0 +1,29 @@
+from typing import Tuple
+
+import numpy as np
+
+
+def gaussian_mixture_moments(
+    w: np.ndarray,  # the mixture weights shape=(N,)
+    mean: np.ndarray,  # the mixture means shape(N, n)
+    cov: np.ndarray,  # the mixture covariances shape (N, n, n)
+) -> Tuple[
+    np.ndarray, np.ndarray
+]:  # the mean and covariance of of the mixture shapes ((n,), (n, n))
+    """Calculate the first two moments of a Gaussian mixture"""
+
+    # mean
+    mean_bar = np.average(mean, axis=0, w)  # TODO: hint np.average using axis and weights argument
+
+    # covariance
+    # # internal covariance
+    cov_int = np.average(cov, axis=0, w)  # TODO: hint, also an average
+
+    # # spread of means
+    # Optional calc: mean_diff =
+    cov_ext = np.average(mean @ mean.T, axis=0, w) + mean_bar @ mean_bar.T  # TODO: hint, also an average
+
+    # # total covariance
+    cov_bar = cov_int + cov_ext  # TODO
+
+    return mean_bar, cov_bar
